75763354b6011b8f0b93dfb3f22561eaab0774d4
cgj3 <cgj3@cms-rg06-78.cms.waikato.ac.nz>
2020-10-17 21:26:28 +1300

    Setup basic threading

--- robot/bin/native/cpu/tramp.d
+++
@@ -1,7 +0,0 @@
-/home/cgj3/RIOT/examples/survivor/robot/bin/native/cpu/tramp.o: \
- /home/cgj3/RIOT/cpu/native/tramp.S /usr/include/stdc-predef.h \
- /home/cgj3/RIOT/examples/survivor/robot/bin/native/riotbuild/riotbuild.h
-
-/usr/include/stdc-predef.h:
-
-/home/cgj3/RIOT/examples/survivor/robot/bin/native/riotbuild/riotbuild.h:
