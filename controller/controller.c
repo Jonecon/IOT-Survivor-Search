408cffd63e64b0dae88f54887f224ef0fa0afee7
meecah <mc211@students.waikato.ac.nz>
2020-10-18 18:23:43 +1300

    Completed all the controller commands

--- controller/controller.c
+++ controller/controller.c
@@ -14,7 +14,7 @@
 
 // DECLARING FUNCTIONS
 extern int _gnrc_netif_config(int argc, char **argv);
-extern int sUp_cmd(int argc, char **argv);
+extern int controller_cmd(int argc, char **argv);
 // extern int sDown_cmd(int argc, char **argv);
 // extern int sLeft_cmd(int argc, char **argv);
 // extern int sRight_cmd(int argc, char **argv);
@@ -26,18 +26,19 @@ extern int getSta_cmd_remote(char* response);
 // DECLARING VARIABLES
 int numRobots = 0;
 char robot_addresses[MAX_ROBOTS][24];
-char stack[MAX_ROBOTS][THREAD_STACKSIZE_MAIN];
+char control_thread_stack[MAX_ROBOTS][THREAD_STACKSIZE_MAIN];
+char auto_thread_stack[MAX_ROBOTS][THREAD_STACKSIZE_MAIN];
 char message[MAX_ROBOTS][20];
-uint8_t buf[16];
+uint8_t buf[255];
 
 /* DEFINING SETS OF COMMANDS FOR THE CONTROLLER */
 static const shell_command_t shell_commands[] = {
-	{"sUp", "Send move up instruction to a robot", sUp_cmd},
-	// {"sDown", "Send move down instruction to a robot", sDown_cmd},
-	// {"sLeft", "Send move left instruction to a robot", sLeft_cmd},
-	// {"sRight", "Send move right instruction to a robot", sRight_cmd},
-	// {"stop", "Send stop instruction to a robot", stop_cmd},
-	// {"getSta", "Send get status instruction to a robot", getSta_cmd},
+	{"sUp", "Send move up instruction to a robot", controller_cmd},
+	{"sDown", "Send move down instruction to a robot", controller_cmd},
+	{"sLeft", "Send move left instruction to a robot", controller_cmd},
+	{"sRight", "Send move right instruction to a robot", controller_cmd},
+	{"stop", "Send stop instruction to a robot", controller_cmd},
+	{"getSta", "Send get status instruction to a robot", controller_cmd},
 	// {"getList", "List robot status", getList_cmd},
 	{NULL, NULL, NULL}
 };
@@ -46,6 +47,7 @@ static const shell_command_t shell_commands[] = {
 
 void *controller_thread_handler(void *arg) {
 
+	/* TO BE DELETED ? */
 	int id = atoi(arg);
 	printf("\nid: %d\n", id);
 
@@ -69,33 +71,36 @@ void *controller_thread_handler(void *arg) {
 		sock_udp_close(&sock);
 		return NULL;
 	}
-	// then the remote port
+
+	// SETTING THE PORT OF THE CONTROLLER
 	remote.port = CONTROLLER_PORT;
 
-	//configure the underlying network such that all packets transmitted will reach a server
+	// configure the underlying network such that all packets transmitted will reach a server
 	ipv6_addr_set_all_nodes_multicast((ipv6_addr_t *)&remote.addr.ipv6, IPV6_ADDR_MCAST_SCP_LINK_LOCAL);
 
 	ssize_t res;
 	while (1) {
+
+		// IF THERE IS NO COMMAND
 		if (strlen(message[id]) == 0) {
 			xtimer_sleep(1);
 			continue;
-		}
-		else
-		{
-			printf("sending: %s\n", message[id] );
+		} else {
 
-			if (sock_udp_send(&sock, message[id], strlen(message[id]), &remote) < 0)
-			{
+			/* TO BE DELETED ? */
+			printf("sending: %s to robot id: %d\n", message[id], id);
+
+			// TRANSMITTING THE MESSAGE TO THE ROBOT[id] WHILE CHECKING
+			if (sock_udp_send(&sock, message[id], strlen(message[id]) + 1, &remote) < 0) {
 				puts("Error sending message");
 				sock_udp_close(&sock);
 				return NULL;
 			}
 
-			// wait for msg
+			// WAITING FOR A RESPONSE
 			sock_udp_ep_t remote;
 			buf[0] = 0;
-			res = sock_udp_recv(&sock, buf, sizeof(buf), 1 * US_PER_SEC, &remote);
+			res = sock_udp_recv(&sock, buf, sizeof(buf), 3 * US_PER_SEC, &remote);
 			if (res < 0) {
 				if (res == -ETIMEDOUT) {
 					puts("Timed out, no response. Message may be lost or delayed.");
@@ -104,7 +109,7 @@ void *controller_thread_handler(void *arg) {
 				}
 			}
 			else {
-				// convert server (i.e. remote) address from ipv6_addr_t to string
+				// CONVERT ROBOT ADDRESS FROM ipv6_addr_t TO STRING
 				char ipv6_addr[IPV6_ADDR_MAX_STR_LEN];
 				if (ipv6_addr_to_str(ipv6_addr, (ipv6_addr_t *)&remote.addr.ipv6, IPV6_ADDR_MAX_STR_LEN ) == NULL) {
 					printf("\nCannot convert server address\n");
@@ -113,13 +118,13 @@ void *controller_thread_handler(void *arg) {
 
 				// ensure a null-terminated string
 				buf[res] = 0;
+
+				/* TO BE DELETED ? */
 				printf("Received from server (%s, %d): \"%s\"\n", ipv6_addr, remote.port, buf);
-				// take parameter from the message
 			}
 
 			// DELETING COMMAND FROM THE MESSAGE
 			strcpy(message[id], "");
-			// printf("Sending %s instruction to robot\n", arg[0]);
 		}
 	}
 
@@ -142,8 +147,11 @@ int main(void) {
 		strcpy(robot_addresses[robotID], token);
 
 		// CREATING THREAD PASSING ID AS PARAMETER
-		thread_create(stack[robotID], sizeof(stack[robotID]), THREAD_PRIORITY_MAIN - 1,
+		thread_create(control_thread_stack[robotID], sizeof(control_thread_stack[robotID]), THREAD_PRIORITY_MAIN - 1,
 			THREAD_CREATE_STACKTEST, controller_thread_handler, (void *)&robotID, "control thread");
+
+		// thread_create(auto_thread_stack[robotID], sizeof(control_thread_stack[robotID]), THREAD_PRIORITY_MAIN - 1,
+		// 	THREAD_CREATE_STACKTEST, controller_thread_handler, (void *)&robotID, "control thread");
 
 		numRobots++;
 		robotID++;
@@ -160,7 +168,7 @@ int main(void) {
 }
 
 /* DECLARING FUNCTIONS */
-int sUp_cmd(int argc, char **argv) {
+int controller_cmd(int argc, char **argv) {
 
 	//IF USER DID NOT PUT ROBOT ID
 	if (argc != 2) {
@@ -172,8 +180,6 @@ int sUp_cmd(int argc, char **argv) {
 	if (robotid <= MAX_ROBOTS - 1) {
 		//SET MESSAGE[robot_id] WITH THE COMMAND TO BE SENT
 		strcpy(message[robotid], argv[0]);
-		printf("Sending %s instruction to robot\n", argv[0]);
-
 	} else {
 		printf("No robot with this ID\n");
 		return 1;
