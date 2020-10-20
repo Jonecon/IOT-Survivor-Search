ec344b5a2144acc2b13c24e9fd8bfa43b5ef2cff
meecah <mc211@students.waikato.ac.nz>
2020-10-20 18:43:13 +1300

    Starting Logic Thread

--- controller/controller.c
+++ controller/controller.c
@@ -15,24 +15,27 @@
 // DECLARING FUNCTIONS
 extern int _gnrc_netif_config(int argc, char **argv);
 extern int controller_cmd(int argc, char **argv);
-// extern int sDown_cmd(int argc, char **argv);
-// extern int sLeft_cmd(int argc, char **argv);
-// extern int sRight_cmd(int argc, char **argv);
-// extern int stop_cmd(int argc, char **argv);
-// extern int getSta_cmd(int argc, char **argv);
-// extern int getList_cmd(int argc, char **argv);
 extern int getSta_cmd_remote(char* response);
 
+struct Point {
+	int x;
+	int y;
+};
+
 // DECLARING VARIABLES
-int numRobots = 0;
 char robot_addresses[MAX_ROBOTS][24];
 char control_thread_stack[MAX_ROBOTS][THREAD_STACKSIZE_MAIN];
 char auto_thread_stack[MAX_ROBOTS][THREAD_STACKSIZE_MAIN];
 char listener_thread_stack[THREAD_STACKSIZE_MAIN];
 char message[MAX_ROBOTS][20];
+
 uint8_t buf[255];
 
 int robotID = 0;
+int numRobots = 0;
+
+struct Point border;
+struct Point position;
 
 /* DEFINING SETS OF COMMANDS FOR THE CONTROLLER */
 static const shell_command_t shell_commands[] = {
@@ -42,6 +45,7 @@ static const shell_command_t shell_commands[] = {
 	{"sRight", "Send move right instruction to a robot", controller_cmd},
 	{"stop", "Send stop instruction to a robot", controller_cmd},
 	{"getSta", "Send get status instruction to a robot", controller_cmd},
+	{"pos", "Send pos instruction to a robot", controller_cmd},
 	// {"getList", "List robot status", getList_cmd},
 	{NULL, NULL, NULL}
 };
@@ -81,18 +85,22 @@ void *controller_thread_handler(void *arg) {
 	ipv6_addr_set_all_nodes_multicast((ipv6_addr_t *)&remote.addr.ipv6, IPV6_ADDR_MCAST_SCP_LINK_LOCAL);
 
 	while (1) {
+
 		// IF THERE IS NO COMMAND
 		if (strlen(message[id]) == 0) {
 			xtimer_sleep(1);
 			continue;
+
 		} else {
+			ssize_t res;
+
 			// IF THERE'S AN ERROR CREATING UDP SOCK
 			if (sock_udp_create(&sock, &local, NULL, 0) < 0) {
 				puts("Error creating UDP sock");
 				return NULL;
 			}
-			ssize_t res;
-			printf("sending: %s to robot id: %d\n", message[id], id);
+			
+			printf("\nSending: %s to robot id: %d\n", message[id], id);
 
 			// TRANSMITTING THE MESSAGE TO THE ROBOT[id] WHILE CHECKING
 			if (sock_udp_send(&sock, message[id], strlen(message[id]) + 1, &remote) < 0) {
@@ -105,6 +113,7 @@ void *controller_thread_handler(void *arg) {
 			sock_udp_ep_t remote;
 			uint8_t buf[255];
 			res = sock_udp_recv(&sock, buf, sizeof(buf), 3 * US_PER_SEC, &remote);
+
 			if (res < 0) {
 				if (res == -ETIMEDOUT) {
 					puts("Timed out, no response. Message may be lost or delayed.");
@@ -128,8 +137,7 @@ void *controller_thread_handler(void *arg) {
 					-Don't need to reply in this thread now
 				*/
 
-
-				//printf("Received from server (%s, %d): \"%s\"\n", ipv6_addr, remote.port, buf);
+				printf("Received from server (%s, %d): \"%s\"\n", ipv6_addr, remote.port, buf);
 				strcpy(message[id], "");
 			}
 			sock_udp_close(&sock);
@@ -140,13 +148,13 @@ void *controller_thread_handler(void *arg) {
 }
 
 void *listener_thread_handler(void* arg){
+
 	//Here so we can display all messages recieved and are able to recieve spontaneous messages.
 	/* if the port is the same as this threads robot
 				Then we have a message to proccess and display
 				if this message
 					THEN
 				etc
-
 	*/
 	(void) arg;
 	sock_udp_ep_t local = SOCK_IPV6_EP_ANY;
@@ -161,10 +169,12 @@ void *listener_thread_handler(void* arg){
 	}
 
 	while (1){
+
 		ssize_t res;
 		sock_udp_ep_t remote;
 		uint8_t buf[255];
 		res = sock_udp_recv(&sock, buf, sizeof(buf), 3 * US_PER_SEC, &remote);
+
 		if (res < 0) {
 			if (res == -ETIMEDOUT) {
 			} else {
@@ -186,9 +196,31 @@ void *listener_thread_handler(void* arg){
 	}
 }
 
-int main(void) {
+void *logic_thread_handler(void *arg) {
+	(void)arg;
 
-	// SETTING UP VARIABLES
+	// DECLARING VARIABLES
+	border.x = NUM_LINES + 1;
+	border.y = NUM_COLUMNS + 1;
+	
+	// MOVING THE ROBOT TO ITS INITIAL POSITION
+	position.x = 0;
+	for (int i = 0; i < numRobots; ++i)
+	{
+		position.y = ((border.y/numRobots)*i);
+		//SET MESSAGE[robot_id] WITH THE COMMAND TO BE SENT
+		// strcpy(message[id], ("pos " + position.x + " " + position.y));
+
+		/* TO BE DELETED ? */
+	printf("Robot %d position (%d, %d)\n",i, position.x, position.y);
+	}
+
+	
+
+	while(1){xtimer_sleep(1);}
+}
+
+int main(void) {
 
 	// GETTING ALL ROBOT IPv6 ADDRESSES
 	char str[24*MAX_ROBOTS];
@@ -200,22 +232,22 @@ int main(void) {
 	// SETTING UP COMMUNICATION THREADS FOR EACH ROBOTS
 	while (token != NULL) {
 		strcpy(robot_addresses[robotID], token);
-		//printf("This is the token: %s\n", token);
 
 		// CREATING THREAD PASSING ID AS PARAMETER
 		thread_create(control_thread_stack[robotID], sizeof(control_thread_stack[robotID]), THREAD_PRIORITY_MAIN - 1,
-			THREAD_CREATE_STACKTEST, controller_thread_handler, NULL, "control thread");
-
-		// thread_create(auto_thread_stack[robotID], sizeof(control_thread_stack[robotID]), THREAD_PRIORITY_MAIN - 1,
-		// 	THREAD_CREATE_STACKTEST, controller_thread_handler, (void *)&robotID, "control thread");
+			THREAD_CREATE_STACKTEST, controller_thread_handler, NULL, "Control Thread");
 
 		numRobots++;
 		robotID++;
 		token = strtok(NULL, ",");
 	}
 
+	thread_create(auto_thread_stack[robotID], sizeof(auto_thread_stack[robotID]), THREAD_PRIORITY_MAIN - 1,
+			THREAD_CREATE_STACKTEST, logic_thread_handler, NULL, "Logic Thread");
+
 	thread_create(listener_thread_stack, sizeof(listener_thread_stack), THREAD_PRIORITY_MAIN - 1,
 		THREAD_CREATE_STACKTEST, listener_thread_handler, NULL, "Listener Thread");
+
 	/*
 	for(unsigned int i = 0; i < (sizeof(robot_addresses)/sizeof(robot_addresses[0])); i++){
 		printf("robot %d address: %s\n", i, robot_addresses[i]);
@@ -232,9 +264,21 @@ int main(void) {
 
 /* DECLARING FUNCTIONS */
 int controller_cmd(int argc, char **argv) {
+	if (argc == 4){
+		int robotid = atoi(argv[1]);
+		if (robotid <= MAX_ROBOTS - 1) {
+		//SET MESSAGE[robot_id] WITH THE COMMAND TO BE SENT
+		sprintf(message[robotid], "%s %s %s", argv[0], argv[2], argv[3]);
+		return 0;
+		}
+	} else {
+		printf("No robot with this ID\n");
+		return 1;
+	}
+	
 
 	//IF USER DID NOT PUT ROBOT ID
-	if (argc != 2) {
+	if ((argc != 2) && (argc != 4)) {
 		printf("usage: %s robot_id\n", argv[0]);
 		return 1;
 	}
